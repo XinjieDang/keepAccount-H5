bd9109977c25072bc7f991059d2cbbdc9b520087
Horizon\XinjieDang <1328791209@qq.com>
2023-03-23 22:50:30 +0800

    feat:测试新的

--- src/components/NewOrder/index.tsx
+++ src/components/NewOrder/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
 export default function index() {
-  return <div>new组件===================================</div>
+  return
+  ;<>
+    <div>测试新的分支</div>
+    <div>new组件===================================</div>
+  </>
 }
