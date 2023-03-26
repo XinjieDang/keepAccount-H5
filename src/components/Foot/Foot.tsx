67bf038f4cdf506bc70dff214302851674e775ed
Horizon\XinjieDang <1328791209@qq.com>
2023-03-27 00:15:21 +0800

    feat:update

--- src/components/Foot/Foot.tsx
+++ src/components/Foot/Foot.tsx
@@ -1,32 +1,10 @@
-// import React, { Component } from 'react'
-// import { connect } from 'react-redux'
-
-// export class Foot extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <ul>
-//           <li>
-//             <div>大家一起来讨论react</div>
-//           </li>
-//         </ul>
-//       </div>
-//     )
-//   }
-// }
-
-// const mapStateToProps = (state: string) => console.log(state)
-
-// const mapDispatchToProps = {}
-
-// export default connect(mapStateToProps, mapDispatchToProps)(Foot)
 import React from 'react'
-
 export default function Foot() {
   return (
     <div>
       Foot,做一下更改--===========================master=================================
       <div>参数从错错错所错所错多错所错</div>
+      <div>我以为我以为唯一我误以为误以为</div>
     </div>
   )
 }
